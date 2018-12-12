bd96c3ea82ee44aa9710743c1cbf550c1eac13d1
Mateusz Larkowski <u1864886@hud.ac.uk>
2018-12-12 19:41:30 +0000

    better format so it fits word

--- src/week1/srala.java
+++ src/week1/srala.java
@@ -4,6 +4,10 @@ public class srala
 {
     public srala()
     {
+        public static void main (String[] args) {
+
+    }Employee sds = new Employee();
+        System.out.println("sds = " + sds);
 
     }
 }
